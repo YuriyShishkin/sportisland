c167443aef1f1c3b74bab61ef5bba31510c357b1
Yuriy Shishkin <ya.shishkin-yuriy@ya.ru>
2021-01-16 13:38:45 +0700

    Настроил WordPress

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -29,7 +29,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '6d0ffa02fe787d4349b31f1326eebdc82bfbf0c2',
+    'reference' => 'a1a73ad7f708bf12a52b539f5d55c6dd80b27ade',
     'name' => '__root__',
   ),
   'versions' => 
@@ -41,7 +41,16 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '6d0ffa02fe787d4349b31f1326eebdc82bfbf0c2',
+      'reference' => 'a1a73ad7f708bf12a52b539f5d55c6dd80b27ade',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.10.0',
+      'version' => '1.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a0357fccad9d1cc1ea0c9a05b8847fbccccb78d',
     ),
     'johnpbloch/wordpress' => 
     array (
@@ -70,12 +79,62 @@ private static $installed = array (
       ),
       'reference' => '237faae9a60a4a2e1d45dce1a5836ffa616de63e',
     ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
     'wordpress/core-implementation' => 
     array (
       'provided' => 
       array (
         0 => '5.6.0',
       ),
+    ),
+    'wpackagist-plugin/advanced-custom-fields' => 
+    array (
+      'pretty_version' => '5.9.4',
+      'version' => '5.9.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'tags/5.9.4',
+    ),
+    'wpackagist-plugin/contact-form-7' => 
+    array (
+      'pretty_version' => '5.3.2',
+      'version' => '5.3.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'tags/5.3.2',
+    ),
+    'wpackagist-plugin/hc-custom-wp-admin-url' => 
+    array (
+      'pretty_version' => '1.4',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'trunk',
+    ),
+    'wpackagist-plugin/rus-to-lat-advanced' => 
+    array (
+      'pretty_version' => '1.1',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'trunk',
     ),
   ),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit6c9fe3b6fa5bfaf838a349f383d81fcb
 {
+    public static $prefixLengthsPsr4 = array (
+        'C' => 
+        array (
+            'Composer\\Installers\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Composer\\Installers\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
+        ),
+    );
+
     public static $prefixesPsr0 = array (
         'j' => 
         array (
@@ -23,6 +37,8 @@ class ComposerStaticInit6c9fe3b6fa5bfaf838a349f383d81fcb
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit6c9fe3b6fa5bfaf838a349f383d81fcb::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit6c9fe3b6fa5bfaf838a349f383d81fcb::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit6c9fe3b6fa5bfaf838a349f383d81fcb::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit6c9fe3b6fa5bfaf838a349f383d81fcb::$classMap;
 
